d8b28339764e3c10f18da5dd94f7a09397d6451f
rae-glazier <glazier.home.life@gmail.com>
2019-11-07 18:32:12 -0600

    axios in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import axios from 'axios';
+
+axios.get(`https://api.nasa.gov/planetary/apod?api_key=xJtBVZqj6zfUfcT621guecx6nn46jNDnydaw2CbC`)
+    .then(response => {
+        console.log('nasa is cool', response);
+    })
+
+
 function App() {
   return (
     <div className="App">
