f308805c10411b672124418e33afb9ab7a3fb2b6
rae-glazier <glazier.home.life@gmail.com>
2019-11-08 09:48:17 -0600

    basics seem to be working

--- components/NasaCard.js
+++ components/NasaCard.js
@@ -4,8 +4,8 @@ const NasaCard = props => {
     return (
         <div className = 'star-list' key = {props.id}>
            <h2>{props.title}</h2>
-           {/* <img src={props.hdurl} alt={props.title}/> */}
-           <p>{props.explanation}</p> 
+           <img src={props.img} alt={props.title}/>
+           <p>{props.description}</p> 
            <p>{props.copyright}</p>
            <p>{props.date}</p>
         </div>
--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -3,9 +3,9 @@ import React from "react";
 const NasaCard = props => {
     return (
         <div className = 'star-list' key = {props.id}>
-           <h2>Photo Title: {props.title}</h2>
-           <img src='{props.hdurl}' alt='{props.title}'></img>
-           <p>{props.explanation}</p> 
+           <h2>{props.title}</h2>
+           <img src={props.img} alt={props.title}/>
+           <p>{props.description}</p> 
            <p>{props.copyright}</p>
            <p>{props.date}</p>
         </div>
